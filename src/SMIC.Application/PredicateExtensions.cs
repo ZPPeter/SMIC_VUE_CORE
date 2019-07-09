60b95c3c4cfb9f027132697d76575207b3612ed8
Peter <edmic@163.com>
2019-07-09 09:11:18 +0800

    Update PredicateExtensions

--- src/SMIC.Application/PredicateExtensions.cs
+++ src/SMIC.Application/PredicateExtensions.cs
@@ -25,5 +25,24 @@ namespace SMIC
             var invokedExpression = Expression.Invoke(expression2, expression1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>(Expression.And(expression1.Body, invokedExpression), expression1.Parameters);
         }
+
+        // {p => (((x.Age == 42) AndAlso (y.Salary == 50)) AndAlso z.FirstName.StartsWith("foo"))}
+        public static Expression<Func<T, bool>> AndAlso<T>(this Expression<Func<T, bool>> expr1,Expression<Func<T, bool>> expr2)
+        {
+            // need to detect whether they use the same
+            // parameter instance; if not, they need fixing
+            ParameterExpression param = expr1.Parameters[0];
+            if (ReferenceEquals(param, expr2.Parameters[0]))
+            {
+                // simple version
+                return Expression.Lambda<Func<T, bool>>(
+                    Expression.AndAlso(expr1.Body, expr2.Body), param);
+            }
+            // otherwise, keep expr1 "as is" and invoke expr2
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(
+                    expr1.Body,
+                    Expression.Invoke(expr2, param)), param);
+        }
     }
 }
